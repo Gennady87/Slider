6b97072867753c49edf9f93109aa95ca3802b028
Gennady87 <77787779+Gennady87@users.noreply.github.com>
2024-02-04 03:01:54 +0700

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Gennady on 04.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
