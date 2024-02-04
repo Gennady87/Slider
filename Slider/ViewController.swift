88502e18ee3fe271ebbd10a9ec96a3deb17dfb95
Gennady87 <77787779+Gennady87@users.noreply.github.com>
2024-02-05 03:45:18 +0700

    Arrangement of elements on the screen

--- Slider/ViewController.swift
+++ Slider/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
     }
 
-
+    @IBAction func changeColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(
+            red: red,
+            green: green,
+            blue: blue,
+            alpha: 1.0
+        )
+    }
+    
 }
 
